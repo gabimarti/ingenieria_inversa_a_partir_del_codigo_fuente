f146a91563cbc3aac568098a903b72648146cd35
Gabriel Marti <1541061+gabimarti@users.noreply.github.com>
2020-05-08 19:44:56 +0200

    prog_c_001

--- src/prog_c_001/main.c
+++ src/prog_c_001/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
+/*
+ * prog_c_001
+ */
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+    char str[] = "hola mundo\n";
+    int a = 65;
+    int b = 66;
+    int c = a + b;
+	printf("%s", str);
+    printf("La suma de a(%d) + b(%d) es = c(%d)\n", a, b, c);
 	return 0;
 }
